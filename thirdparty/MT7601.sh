4b629e80c9aef8f43b318eb134de1e1d3a7d0159
Robert Nelson <robertcnelson@gmail.com>
2014-09-22 11:14:26 -0500

    MT7601.sh, use git repo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- thirdparty/MT7601.sh
+++ thirdparty/MT7601.sh
@@ -1,42 +1,39 @@
 	file_dir="https://rcn-ee.net/deb/${release}-${dpkg_arch}/${FTPDIR}/"
 	if [ "x${SUBARCH}" = "xomap-psp" ] ; then
-		mt7601="DPO_MT7601U_LinuxSTA_3.0.0.4_20130913"
+		package="farm_mt7601u"
 
 		cleanup_third_party
 
 		echo '#!/bin/sh' > /build/buildd/thirdparty
 
-		${wget_dl} ${local_http}/MT7601/${mt7601}.tar.bz2
-		if [ -f /build/buildd/${mt7601}.tar.bz2 ] ; then
-			mkdir -p /build/buildd/${mt7601}
-			mv /build/buildd/${mt7601}.tar.bz2 ${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/
-			cd ${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/
-			tar xf ${mt7601}.tar.bz2
-			rm -rf ${mt7601}.tar.bz2 || true
+		cd ${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/
+		git clone https://github.com/rcn-ee/${package}.git
 
-			#Quiet dmesg noise...
-			sed -i -e 's:RT_DEBUG_TRACE:RT_DEBUG_WARN:g' ./${mt7601}/os/linux/rt_linux.c
+		cd /build/buildd/
+		git clone https://github.com/rcn-ee/${package}.git
 
-			cd /build/buildd/${mt7601}
-			make_mt7601="make ARCH=arm CROSS_COMPILE= LINUX_SRC=/build/buildd/linux-src all"
-			schroot -c ${release}-${dpkg_arch} -u ${chroot_user} -- ${make_mt7601}
-		fi
-		file_upload="${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/${mt7601}/RT2870STA.dat"
+		cd /build/buildd/${package}/src/
+		schroot -c ${release}-${dpkg_arch} -u ${chroot_user} -- ../build.sh
+
+		#make sure the module was built
+		file_upload="${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/${package}/src/os/linux/mt7601Usta.ko"
 		if [ -f ${file_upload} ] ; then
-			generic_upload
-			echo "mkdir -p /etc/Wireless/RT2870STA/" >> /build/buildd/thirdparty
-			echo "wget ${file_dir}RT2870STA.dat -O /etc/Wireless/RT2870STA/RT2870STA.dat" >> /build/buildd/thirdparty
-		fi
-		file_upload="${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/${mt7601}/os/linux/mt7601Usta.ko"
-		if [ -f ${file_upload} ] ; then
+			file_upload="${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/${package}/src/RT2870STA.dat"
+			if [ -f ${file_upload} ] ; then
+				generic_upload
+				echo "mkdir -p /etc/Wireless/RT2870STA/" >> /build/buildd/thirdparty
+				echo "wget ${file_dir}RT2870STA.dat -O /etc/Wireless/RT2870STA/RT2870STA.dat" >> /build/buildd/thirdparty
+			fi
+
+			file_upload="${CHROOT_DIR}/${release}-${dpkg_arch}/build/buildd/${package}/src/os/linux/mt7601Usta.ko"
 			generic_upload
 			echo "mkdir -p /lib/modules/${KERNEL_UTS}/kernel/drivers/net/wireless/" >> /build/buildd/thirdparty
 			echo "wget ${file_dir}mt7601Usta.ko -O /lib/modules/${KERNEL_UTS}/kernel/drivers/net/wireless/mt7601Usta.ko" >> /build/buildd/thirdparty
 			echo 'echo "mt7601Usta" > /etc/modules-load.d/mt7601.conf' >> /build/buildd/thirdparty
-		fi
 
-		file_upload="/build/buildd/thirdparty"
-		generic_upload
+			file_upload="/build/buildd/thirdparty"
+			generic_upload
+		fi
 
 		cleanup_third_party
 	fi
